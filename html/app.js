d6333d0cb34e023b90e76d01eaa849a083ad607f
bal97 <davide.balestra2@studio.unibo.it>
2018-11-26 15:50:08 +0100

    trying new routes

--- html/app.js
+++ html/app.js
@@ -2,9 +2,21 @@ var express = require('express');
 var app = express();
 var path = require("path");
 
-//Load index.html
+
+/* Main request, load index.html file, identified as
+ * Request → site1840.tw.cs.unibo.it/
+ */
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname+'/index.html'));
+	});
+
+
+/* Search video on youtube based on search field
+ * Request → site1840.tw.cs.unibo.it/search_videos/$query
+ */
+app.get('/search_videos/:query', function(req, res){
+	
+	res.send("Hello, you're looking for "+req.params.query);
 	});
 
 app.listen(8000);
