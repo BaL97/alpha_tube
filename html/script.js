c24feefec427a57bc3c36de14e26bd827c6f577b
danielepolidori <daniele.polidori2@studio.unibo.it>
2019-03-02 18:28:08 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,5 +1,4 @@
-//var watching="ubUdtowLIZo";
-var site = "http://daniele.polidori2.tw.cs.unibo.it/";
+var site = "http://site1840.tw.cs.unibo.it/";
 var starter_list;
 var search_list;
 var cronology = new Array();
@@ -7,12 +6,25 @@ var timer;
 var player;
 var added=false;
 var counter=0;
-var recommender='starter';
+var recommender='Starter';
 var global_pop = new Array();
-var artist = "" ;
-var track = "" ;
-var titleParsed = "" ;
-var titleParsed_weak = "" ;
+var global_relative = new Array();
+var h_state = false;
+var artist = "";
+var track = "";
+var titleParsed = "";
+var titleParsed_weak = "";
+var genre = "";
+
+var popRelLoc = new Array();
+
+
+function navHandler(){
+	h_state = true;
+	if(history.state)	loadvideo(history.state);
+}
+
+window.addEventListener("popstate", navHandler, false);
 
 function setRec(rec){
 	recommender=rec;
@@ -27,24 +39,35 @@ function onYouTubePlayerAPIReady(){
 }
 
 function deleteHistory(){
-localStorage.removeItem('cronology');
-cronology = [];
-printCronology(cronology, '#rec');
+	localStorage.setItem('watching', 'ubUdtowLIZo');
+	localStorage.removeItem('cronology');
+	cronology = [];
+	printCronology(cronology, '#rec');
 }
 
 function onPlayerStateChange(event){
-        if (event.data==1){
-                timer = setInterval(function(){
-                        if(player.getCurrentTime()>=15) {
-                                if(counter==0){
+	if (event.data==1){
+		timer = setInterval(function(){
+
+			if(player.getCurrentTime()>=15) {
+				if(counter==0){
 					//add to cronology
+				if((cronology)&&(cronology.length>0)){
+					if(cronology[0].videoId != localStorage.getItem('watching')){
+						var idPred = cronology[0].videoId;
+						var idSucc = localStorage.getItem('watching');
+						$.get(site+"addRelative/"+idPred+"/"+idSucc+"/"+recommender, function(data){
+                                        		console.log(data);
+						});
+					}
+				}
 					addToCronology(localStorage.getItem('watching'));
 					printCronology(cronology, '#rec');
-					}
+				}
 				counter++;
-                        }
-                }, 1000);
-        }
+			}
+		}, 1000);
+	}
 }
 		
 function isInCronology(v){
@@ -81,54 +104,61 @@ function addToCronology(video){
 			});
 		}
 
-		function loadvideo(video){
-			if(localStorage.getItem('cronology')){
-				cronology = JSON.parse(localStorage.getItem('cronology'));
-				printCronology(cronology, '#rec');
-			}
-			localStorage.setItem('watching', video);
-			//updateWatching(video);
-			counter=0;
-			//watching=video;
-
-			player.loadVideoById(localStorage.getItem('watching'));
-
-			// Azzero il contenuto della sezione Info del video
-			$("#info").html('');
-
-			// Azzero le variabili che saranno riusate per la nuova pulizia del titolo
-			artist = "" ;
-			track = "" ;
-			titleParsed = "" ;
-			titleParsed_weak = "" ;
-
-			$.get(site+"listvideos/"+video, function(data){
-				$("#titVid").html('<h4 id="titVid" class="card-title">'+data.items[0].snippet.title+'</h4>');			//VIDEO TITLE
-				$("#descVid").html('<p id="descVid" class="card-text">'+data.items[0].snippet.description+'</p>');		//VIDEO DESC 
-				$('#commVid').html('Attendere il caricamento...');		//WAIT FOR COMMENTS...
-
-				puliziaTitolo(data.items[0].snippet.title);
-
-				console.log("fine");
+function loadvideo(video){
+	var foundRelative = false;
+	if(localStorage.getItem('cronology')){
+		cronology = JSON.parse(localStorage.getItem('cronology'));
+		printCronology(cronology, '#rec');
+	}
+	$.when (
+		localStorage.setItem('watching', video),
+		counter=0,
+		player.loadVideoById(localStorage.getItem('watching')) ).then(function(){
+			$.get(site+"getRelatives/"+localStorage.getItem('watching'), function(data){
+				for(i in data){
+                                	if(localStorage.getItem('watching')==data[i].id){
+                                	        foundRelative = true;
+                                        	printCronology(data[i].succ, '#LPR');
+                                	}
+                        	}       
+                        	if(!foundRelative) 
+                                	$('#LPR').html('No videos was found');
 			});
+		});
+	if(!h_state)    history.pushState(localStorage.getItem('watching'), "");
 
+	genre="";
+	artist = "";
+	track = "";
+	titleParsed = "";
+	titleParsed_weak = "";
 
-			$.get(site+"getstat/"+localStorage.getItem('watching'), function(data){
-			$("#info").append('<p>'+'VIEW: '+ data.statistics.viewCount  +'</p>'+'<p>'+'LIKE: '+ data.statistics.likeCount  +'</p>'+'<p>'+'DISLIKE: '+ data.statistics.dislikeCount +'</p>');
+	$.get(site+"listvideos/"+video, function(data){
+		$("#titVid").html('<h4 id="titVid" class="card-title">'+data.items[0].snippet.title+'</h4>');			//VIDEO TITLE
+		$("#descVid").html('<p id="descVid" class="card-text">'+data.items[0].snippet.description+'</p>');		//VIDEO DESC 
+		$('#commVid').html('Attendere il caricamento...');		//WAIT FOR COMMENTS...
+		$('#info').html('Attendere il caricamento...');
+		puliziaTitolo(data.items[0].snippet.title);
+		search_wiki(data.items[0].snippet.title);	
+	});
+			
+	$.get(site+"getcomments/"+localStorage.getItem('watching'), function(data){      		//ASK SERVER FOR COMMENTS TO watching VIDEO
+        	$('#commVid').html('');
+            	for (i in data){
+            		$('#commVid').append('<p><b>' + data[i].snippet.topLevelComment.snippet.authorDisplayName + '</b><br><br>' + data[i].snippet.topLevelComment.snippet.textDisplay + '</p><hr>');
+            	}
+	});
+
+	related();
+	fvitali();
+	h_state = false;
+	if(recommender == 'Global Relative')	globalRelative();
+	
+	$.when(artistSimilarity(), genreFind() ).then(function(){ $.get(site+"getstat/"+localStorage.getItem('watching'),function(data){                
+        	$('#info').html('<p>'+'VIEW: '+ data.statistics.viewCount  + '</p>' + '<p>' + 'LIKE: ' + data.statistics.likeCount  + '</p>' + '<p>' + 'DISLIKE: ' + data.statistics.dislikeCount + '</p>' + '<p>' + 'ARTIST: ' + artist + '</p>' + '<p>' + 'GENRE: '+ genre + '</p>'+'<p>'+'TRACK: '+ track + '</p>');
 			});
-
-
-			 $.get(site+"getcomments/"+localStorage.getItem('watching'), function(data){      		//ASK SERVER FOR COMMENTS TO watching VIDEO
-            			$('#commVid').html('');
-            			for (i in data){
-            				$('#commVid').append('<p><b>' + data[i].snippet.topLevelComment.snippet.authorDisplayName + '</b><br><br>' + data[i].snippet.topLevelComment.snippet.textDisplay + '</p><hr>');
-            			}
-			});
-			//global_pop = [];
-			//globalPopularity();
-			related();
-			fvitali();
-		}
+		});
+}		
 		
 
 
@@ -138,10 +168,9 @@ function addToCronology(video){
 			$("#"+section).html('<div class="container">');
 			for(i=0; i<j.length; i++){
   				$.get(site+"listvideos/"+j[i].videoID, function(data){
-                        	$("#"+section).append('<div class="row my-1">'+'<div class="col-sm-4">'+'<a href="#video" onclick="loadvideo('+ api +data.items[0].id + api+ ')">'+
+                        	$("#"+section).append('<div class="row my-1">'+'<div class="col-sm-4" onclick="loadvideo('+ api +data.items[0].id + api+ ')">'+
                                                          '<img class="img-fluid" src=" '+data.items[0].snippet.thumbnails.medium.url +'">'+
-                                	                 '</a>'+
-                                        	         '</div>'+
+                                	                 '</div>'+
                                          		 '<div class="col-md-8">'  +
 						  	'<p>'+data.items[0].snippet.title+'</p>' + '</div>'+'</div>');
 			}); 
@@ -149,27 +178,27 @@ function addToCronology(video){
 			$("#"+section).append('</div>');
 		}
 
-		function starter(){
-			if(localStorage.getItem('watching'))
-				loadvideo(localStorage.getItem('watching'));
-			else	
-				loadvideo('ubUdtowLIZo');			
-			$.get(site+"localPop/default/1/starter/date", function(data){
-				printCronology(data,'#LPA');
-                        });
+function starter(){
+	if(localStorage.getItem('watching'))
+		loadvideo(localStorage.getItem('watching'));
+	else	
+		loadvideo('ubUdtowLIZo');			
+	$.get(site+"localPop/default/1/starter/date", function(data){
+		printCronology(data,'#LPA');
+        });
 
-			jQuery.ajax({
-				type: "GET",
-				url: "http://site1825.tw.cs.unibo.it/video.json",
-				dataType: "json",
-				success: function(res){
-					starter_list=res;
-					printVideos(starter_list, 'start');
-				},
-				error: function(){
-					alert('error');}
-				});
-			}
+	jQuery.ajax({
+		type: "GET",
+		url: "http://site1825.tw.cs.unibo.it/video.json",
+		dataType: "json",
+		success: function(res){
+			starter_list=res;
+			printVideos(starter_list, 'start');
+		},
+		error: function(){
+			console.log('error');}
+		});
+}
 
 		function starterList(){
 			printVideos(starter_list, 'starter');
@@ -185,35 +214,38 @@ function addToCronology(video){
 					printVideos(res.recommended, 'fv');
 				},
 				error: function(){
-					alert('error');}
+					console.log('error');}
 				});
 		}
 	
 		function printSearch(list, section){
 			var i=0;
-			var api = "'";
-			$("#"+section).html('<div class="container">');
-			for(i in list){
-				$("#"+section).append('<div class="row my-1">'+'<div class="col-sm-4">'+'<a href="#video" onclick="loadvideo('+ api +list[i].id.videoId + api+ ')">'+'<img class="img-fluid" src=" '+ list[i].snippet.thumbnails.medium.url +'">'+'</a>'+'</div>'+'<div class="col-md-8">' + '<p>'+list[i].snippet.title+'</p>' + '</div>'+'</div>');
-			}
-			$("#"+section).append('</div>');
-			i=0;
+                        var api = "'";
+                        $("#"+section).html('<div class="container">');
+                        for(i in list){
+                                $("#"+section).append('<div class="row my-1">'+'<div class="col-sm-4" onclick="loadvideo('+ api +list[i].id.videoId + api+ ')">'+'<img class="img-fluid" src=" '+ list[i].snippet.thumbnails.medium.url +'">'+'</div>'+
+                                                         '<div class="col-md-8">'  +
+                                                        '<p>'+list[i].snippet.title+'</p>' + '</div>'+'</div>');
+                        }
+                        $("#"+section).append('</div>');
+                        i=0;
+
 		}
 		
 		function searchVideos(q, section){
 			if(!section) section='search';
 			if(q){
-				jQuery.ajax({
-                	type: "GET",
-                	url: site+"ytsearch/"+q,
-                	dataType: "json",
-                	success: function(res){
+					jQuery.ajax({
+                                	type: "GET",
+                                	url: site+"ytsearch/"+q,
+                                	dataType: "json",
+                                	success: function(res){
 						search_list=res;
 						printSearch(search_list, section);
-                	},
-           			error: function(){
-                    	alert('error');}
-           	 	});
+                                	},
+                               		 error: function(){
+                                        	console.log('error');}
+                               	 	});
 			}
 		}
 
@@ -223,9 +255,8 @@ function addToCronology(video){
 			jQuery.ajaxSetup({async:false});
 			for(i=0; i<items.length; i++){
                                 $.get(site+"listvideos/"+items[i].videoId, function(data){
-                                $(section).append('<div class="row my-1">'+'<div class="col-sm-4">'+'<a href="#video" onclick="loadvideo('+ api +data.items[0].id + api+ ')">'+
+                                $(section).append('<div class="row my-1">'+'<div class="col-sm-4" onclick="loadvideo('+ api +data.items[0].id + api+ ')">'+
                                                          '<img class="img-fluid" src=" '+data.items[0].snippet.thumbnails.medium.url +'">'+
-                                                         '</a>'+
                                                          '</div>'+
                                                          '<div class="col-md-8">'  +
                                                         '<p>'+data.items[0].snippet.title+'</p>' + '</div>'+'</div>');
@@ -252,7 +283,7 @@ function addToCronology(video){
 				printSearch(res, 'rel');
 			},
 			error: function(){
-				alert('error');}
+				console.log('error');}
 			});
 		}
 
@@ -273,7 +304,7 @@ function search_wiki(str){
 			search_wiki_content(ris.query.search[0].title);
 		},
 		error: function(err){
-			$('#wikiVid').html('No document was found');
+			$('#wikiVid').html('No document was founs');
 		}
 	});
 }
@@ -305,34 +336,135 @@ function isInGlobal(videoId){
 	return false;
 }
 
-/*function globalPopularity(){
-	var object;
-	var selected;
-	var flag = false;
-	var j = 0;
-	var number = new Array("1828","1838","1839","1846","1847","1831","1827","1848","1849","1851","1823","1863","1834","1904","1862","1905");
-	$.when(
-		//for (var i=0; i<number.length; i++){
-		$.get("http://site"+number[i]+".tw.cs.unibo.it/globpop?id=YYYYY", function(data){
-			while((!flag)&&(j<data.recommended.length)){
-				selected = data.recommended[j];	
-				if (!isInGlobal(selected.videoId)){	
-					object = {
-						"videoId": selected.videoId,
-						"prevalentReason": selected.prevalentReason
-					}
-					global_pop.push(object);
-					flag=true;
-				}
-				j++;
-			}
-			flag=false;
-			j=0;
-		});).then(function (){
-		alert(JSON.stringify(global_pop));
-	});
+function isInRelative(videoId){
+        for(var i in global_relative){
+                if(global_relative[i].videoId==videoId)
+                        return true;
+        }
+        return false;
+}
 
-}*/
+function addToGlobalPop(video){
+	//esclude chi restituisce un json con campi non conformi alle specifiche
+	if(video[0].videoId){
+		while((global_pop.length<20)&&(video)&&(video.length!=0)){
+			var object = video.shift();
+			if(!isInGlobal(object.videoId))
+				global_pop.push(object);
+		}
+	}
+}
+
+function relativeApiRequest(site){
+	var APIurl = "http://site"+site+".tw.cs.unibo.it/globpop?id="+localStorage.getItem('watching');
+	$.ajax({
+		type: "GET",
+		url: APIurl,
+		success: function(res){
+                        addToGlobalPop(res.recommended);
+                },
+		error: function(err){
+                        console.log('Errore di richiesta API: '+err);
+                }
+	});
+}
+
+function apiRequest(site){
+        var APIurl = "http://site"+site+".tw.cs.unibo.it/globpop?id=YYYYYY";
+        $.ajax({
+                type: "GET",
+                url: APIurl,
+                success: function(res){
+                        addToGlobalPop(res.recommended);
+                },
+                error: function(err){
+                        console.log('Errore di richiesta API: '+err);
+                }
+        });
+}
+
+function globalPopularity(){
+	$("#GPA").html("Loading...");
+	global_pop = [];
+	$.when(
+                apiRequest('1823'),
+                apiRequest('1828'),
+                apiRequest('1838'),
+                apiRequest('1839'),
+                apiRequest('1846'),
+                apiRequest('1847'),
+                apiRequest('1831'),
+                apiRequest('1827'),
+                apiRequest('1848'),
+                apiRequest('1849'),
+                apiRequest('1851'),
+                apiRequest('1863'),
+                apiRequest('1834'),
+                apiRequest('1904'),
+                apiRequest('1862'),
+                apiRequest('1905')
+                //apiRequest('1864') STRINGA NON JSON
+		).then(function(){
+			setTimeout(function (){
+				printCronology(global_pop, "#GPA");
+			}, 2000);
+		});
+}
+
+function addToGlobalRelative(video){
+	//esclude chi restituisce un json con campi non conformi alle specifiche
+        if(video[0].videoId){
+                while((global_relative.length<20)&&(video)&&(video.length!=0)){
+                        var object = video.shift();
+                        if(!isInRelative(object.videoId))
+                                global_relative.push(object);
+                }
+        }
+}
+
+function apiRelative(site){
+	var APIurl = "http://site"+site+".tw.cs.unibo.it/globpop?id="+localStorage.getItem('watching');
+	$.ajax({
+                type: "GET",
+                url: APIurl,
+                success: function(res){
+                        addToGlobalRelative(res.recommended);
+                },
+                error: function(err){
+                        console.log('Errore di richiesta API: '+err);
+                }
+        });
+
+}
+
+function globalRelative(){
+	$("#GPR").html("Loading...");
+	global_relative = [];
+	$.when(
+		apiRelative('1823'),
+                apiRelative('1828'),
+                apiRelative('1838'),
+		apiRelative('1839'),
+                apiRelative('1846'),
+                apiRelative('1847'),
+                apiRelative('1831'),
+                apiRelative('1827'),
+                apiRelative('1848'),
+                apiRelative('1849'),
+                apiRelative('1851'),
+                apiRelative('1863'),
+                apiRelative('1834'),
+                apiRelative('1904'),
+                apiRelative('1862'),
+                apiRelative('1905')
+	).then(function(){
+		setTimeout(function(){
+			printCronology(global_relative, "#GPR");
+		}, 2000);
+	});
+}
+
+
 
 /*
 *	Description: Funzione che data una stringa fa il parsing e determina artista e titolo della canzone, se questo è possibile
@@ -518,7 +650,7 @@ function checkIsTrack(array, index, check){
 function dataCheck(array, index, check){
 
 	// se i dati sono utilizzabili allora viene salvato il tutto
-	if(check.isArtist ^ check.isTrack){	// '^' corrisponde all'operazione di XOR (true solo se soltanto uno dei due e' true)
+	if(check.isArtist ^ check.isTrack){
 
 		if(check.isArtist){
 
@@ -547,9 +679,6 @@ function dataCheck(array, index, check){
 			}
 		}
 
-		$('#info').append("<p>ARTIST: " + artist + "</p>");
-		$('#info').append("<p>TRACK: " + track + "</p>");
-
 		search_wiki(artist);
 	}
 	else if(index < 3){
@@ -566,18 +695,14 @@ function dataCheck(array, index, check){
 	}
 	else{
 
-		$('#info').append("<p>ARTIST: -Not found- </p>");
-		$('#info').append("<p>TRACK: -Not found- </p>");
-
 		$('#wikiVid').html("Sorry! We couldn't find any information about this artist.");
 	}
 }
 
 function artistSimilarity(){
-
 	if(artist != ""){
 
-		$.ajax({
+		return $.ajax({
 			url: site + "ytsearch/" + artist ,
 			method: 'GET',
 			dataType: "json",
@@ -612,9 +737,7 @@ function artistSimilarity(){
 *	- "code" e' una stringa che identifica un genere di un video YouTube.
 */
 function codeToGenre(code) {
-
 	var genre = '' ;
-
 	switch(code)
 	{
 		case "/m/05fw6t":
@@ -685,11 +808,10 @@ function codeToGenre(code) {
 }
 
 function genreFind(){
-
 	var genreInfo = [] ;
-	var genre = '' ;
+	var gen = '' ;
 
-	$.ajax({
+	return $.ajax({
 		url: site + "genreFind/" + localStorage.getItem('watching') ,
 		method: 'GET',
 		dataType: "json",
@@ -699,13 +821,13 @@ function genreFind(){
 
 			$.each(genreInfo, function(index, object){
 				
-				genre = codeToGenre(object) ;
+				gen = codeToGenre(object) ;
 			});
 
-			if(genre == '')
-				genre = 'Music' ;
-
-			genreSimilarity(genre);
+			if(gen == '')
+				gen = 'Music' ;
+			genre = gen;
+			genreSimilarity(gen);
 		},
 		error: function(err){
 
@@ -716,7 +838,6 @@ function genreFind(){
 }
 
 function genreSimilarity(genre_code){
-
 	$.ajax({
 		url: site + "genreSim/" + genre_code ,
 		method: 'GET',
